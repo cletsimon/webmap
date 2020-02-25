5842b073b45058b366180c126e054624e9d64b1d
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 16:30:19 +0800

    adding color for population

--- map1.py
+++ map1.py
@@ -32,7 +32,8 @@ for lt, ln, n, p, el in zip(lat, lon, name, province, elev):
     iframe = folium.IFrame(html=html % (n, n, el, p), width=200, height=100)
     fg.add_child(folium.CircleMarker(location=[lt, ln], radiues=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 
