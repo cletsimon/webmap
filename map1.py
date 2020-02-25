22471f40dff146a0d2f2fd2ea5072512a7d0d80c
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 15:22:31 +0800

    add volcanoe names

--- map1.py
+++ map1.py
@@ -5,13 +5,14 @@ import pandas
 data = pandas.read_csv("active_volcanoes.csv")
 lat = list(data["Latitude"])
 lon = list(data["Longitude"])
+name = list(data["Name of Volcano"])
 
 map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
 # create a feture group for the child
 fg = folium.FeatureGroup(name="My Map")
 # add a marker
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="I'm a marker", icon=folium.Icon(color='green')))
+for lt, ln, n in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=n, icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[14.598323, 120.986769], popup="Manila", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Cebu City", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[7.194696, 125.468886], popup="Davao City", icon=folium.Icon(color='green')))
