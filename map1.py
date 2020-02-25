80492e989ad65a0b242a7e5444aaa8ce2814554d
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 15:17:10 +0800

    add PH active volcanoes

--- map1.py
+++ map1.py
@@ -1,11 +1,17 @@
 import folium
-map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
+import pandas
 
+# use data from csv
+data = pandas.read_csv("active_volcanoes.csv")
+lat = list(data["Latitude"])
+lon = list(data["Longitude"])
+
+map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
 # create a feture group for the child
 fg = folium.FeatureGroup(name="My Map")
 # add a marker
-for coordinates in [[14.598323, 120.986769], [10.314586, 123.887834], [7.194696, 125.468886]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="I'm a marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="I'm a marker", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[14.598323, 120.986769], popup="Manila", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Cebu City", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[7.194696, 125.468886], popup="Davao City", icon=folium.Icon(color='green')))
