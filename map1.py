7f51d9cdd242f3791024630367cf672ba4f24342
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 14:41:10 +0800

    add a marker

--- map1.py
+++ map1.py
@@ -1,3 +1,10 @@
 import folium
 map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
+
+# create a feture group for the child
+fg = folium.FeatureGroup(name="My Map")
+# add a marker
+fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Hey, this is Cebu City", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
 map.save("Map1.html")
