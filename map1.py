6b13b6859cc99d9312acb6bdf5a9ebe8e049c6af
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 15:33:29 +0800

    update the popup using iframe

--- map1.py
+++ map1.py
@@ -6,13 +6,21 @@ data = pandas.read_csv("active_volcanoes.csv")
 lat = list(data["Latitude"])
 lon = list(data["Longitude"])
 name = list(data["Name of Volcano"])
+province = list(data["Province"])
+
+html = """<h4>Volcano information:</h4>
+Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+<br>
+Province: %s
+"""
 
 map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
 # create a feture group for the child
 fg = folium.FeatureGroup(name="My Map")
 # add a marker
-for lt, ln, n in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=n, icon=folium.Icon(color='green')))
+for lt, ln, n, p in zip(lat, lon, name, province):
+    iframe = folium.IFrame(html=html % (n, n, p), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[14.598323, 120.986769], popup="Manila", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Cebu City", icon=folium.Icon(color='green')))
 #fg.add_child(folium.Marker(location=[7.194696, 125.468886], popup="Davao City", icon=folium.Icon(color='green')))
