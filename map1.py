5a03434c59fe45de9c092407f7f7c98217ffc394
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 16:39:55 +0800

    remove unused files

--- map1.py
+++ map1.py
@@ -26,15 +26,20 @@ Province: %s
 
 map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen Terrain")
 # create a feture group for the child
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Active Volcanoes")
 # add a marker
 for lt, ln, n, p, el in zip(lat, lon, name, province, elev):
     iframe = folium.IFrame(html=html % (n, n, el, p), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radiues=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radiues=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
-style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'yellow' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+# add layer control
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
