e8df5e60a32186b7ad8fd95dcc5a3737785c1097
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-25 14:47:19 +0800

    multiple marker using for loop

--- map1.py
+++ map1.py
@@ -4,7 +4,11 @@ map = folium.Map(location=[10.314586, 123.887834], zoom_start=6, tiles = "Stamen
 # create a feture group for the child
 fg = folium.FeatureGroup(name="My Map")
 # add a marker
-fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Hey, this is Cebu City", icon=folium.Icon(color='green')))
+for coordinates in [[14.598323, 120.986769], [10.314586, 123.887834], [7.194696, 125.468886]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="I'm a marker", icon=folium.Icon(color='green')))
+#fg.add_child(folium.Marker(location=[14.598323, 120.986769], popup="Manila", icon=folium.Icon(color='green')))
+#fg.add_child(folium.Marker(location=[10.314586, 123.887834], popup="Cebu City", icon=folium.Icon(color='green')))
+#fg.add_child(folium.Marker(location=[7.194696, 125.468886], popup="Davao City", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
